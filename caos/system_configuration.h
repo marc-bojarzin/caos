b13b62ec2bd8e370abb61d220844ec87ab8186b9
Marc Bojarzin <mail@bojarzin.de>
2017-06-20 08:46:29 +0200

    interim

---
+++ caos/system_configuration.h
@@ -0,0 +1,15 @@
+#ifndef SYSTEM_CONFIGURATION_H
+#define SYSTEM_CONFIGURATION_H
+
+
+namespace caos {
+
+class system_configuration
+{
+public:
+    system_configuration();
+};
+
+} // namespace caos
+
+#endif // SYSTEM_CONFIGURATION_H
