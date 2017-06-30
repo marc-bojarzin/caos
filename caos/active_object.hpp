3070a333f0b58b40a724a2635d91a0e38d166d1e
Marc Bojarzin <mail@bojarzin.de>
2017-06-30 18:01:15 +0200

    interim

---
+++ caos/active_object.hpp
@@ -0,0 +1,15 @@
+#ifndef ACTIVE_OBJECT_H
+#define ACTIVE_OBJECT_H
+
+
+namespace caos {
+
+class active_object : public caos::local_active_object
+{
+public:
+    active_object();
+};
+
+} // namespace caos
+
+#endif // ACTIVE_OBJECT_H
---
+++ src/active_object.cpp
@@ -0,0 +1,10 @@
+#include "active_object.h"
+
+namespace caos {
+
+active_object::active_object()
+{
+
+}
+
+} // namespace caos
